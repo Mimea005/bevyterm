24f63c4a9738161df5f27f3f477bca2711b28e3b
Mimea005 <30933848+Mimea005@users.noreply.github.com>
2023-03-26 15:11:42 +0200

    Removed a println that shouldn't have been there.

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,25 +1,27 @@
 use std::io::stdout;
 
 use bevy::{prelude::*, app::AppExit};
-use bevyterm::{TerminalPlugin, components::CrosstermWindow, event::KeyEvent};
+use bevyterm::{TerminalPlugin, components::CrosstermWindow, event::KeyEvent, error_handling::crash_on_err};
 use crossterm::{event::{KeyCode, KeyModifiers}, queue, cursor::MoveTo, style::Print};
+use anyhow::Result;
 
 fn main() {
     App::new()
         .add_plugins(MinimalPlugins)
         .add_plugin(TerminalPlugin)
-        .add_system(hello_world)
+        .add_system(hello_world.pipe(crash_on_err))
     .run();
 }
 
 
-fn hello_world(mut exit: EventWriter<AppExit>, mut key_events: EventReader<KeyEvent>, win: Query<&CrosstermWindow>) {
+fn hello_world(mut exit: EventWriter<AppExit>, mut key_events: EventReader<KeyEvent>, win: Query<&CrosstermWindow>) -> Result<()> {
     let win = win.single();
-    queue!(stdout(), MoveTo(win.width()/2, win.height()/2), Print("Hello World!")).unwrap();
-    println!("hello world");
+    queue!(stdout(), MoveTo(win.width()/2, win.height()/2), Print("Hello World!"))?;
     for key in key_events.iter() {
         if key.code == KeyCode::Char('c') && key.modifiers.contains(KeyModifiers::CONTROL) {
             exit.send(AppExit)
         }
     }
+
+    Ok(())
 }
